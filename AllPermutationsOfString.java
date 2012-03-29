29c44909186cefe9537746d9b68f9fe5815bd731
Sreeprasad <sreeprasad.sp@gmail.com>
2012-03-29 02:07:32 -0400

    all permutations of string

---
+++ AllPermutationsOfString.java
@@ -0,0 +1,17 @@
+public class AllPermutationsOfString{
+
+public static void perm(String prefix, String s){
+	int n=s.length();
+	if(n==0) System.out.println(prefix);
+	else
+		for(int i=0;i<n;i++)
+			perm(prefix+s.charAt(i),  s.substring(0,i)+s.substring(i+1,n) );
+}
+
+
+public static void main(String an[]){
+	perm("","abc");
+	
+}
+
+}
